4cc1973b6d18901a5e8ca0fe027755b624316f0c
Bilaal Ali <Bilaalali44@gmail.com>
2022-10-15 12:35:50 -0600

    Initial Commit

---
+++ Shared/TrivaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivaGameApp.swift
+//  Shared
+//
+//  Created by Bilaal Ali on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct TrivaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
